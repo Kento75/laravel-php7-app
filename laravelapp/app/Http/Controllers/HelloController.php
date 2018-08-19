5324b3720f6977b70a57c412466c950241da55ca
Kento75 <kento2github@gmail.com>
2018-08-20 06:25:46 +0900

    Fix Use Blade Template

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -8,25 +8,10 @@ use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-    public function index(Request $request, Response $response) {
-$html = <<< EOF
-<html>
-<head>
-<style>
-body { font-size: 16px; color: #999; }
-h1 { font-size: 100pt; text-align: right; color: #eee; margin: -40px 0px -50px 0px; }
-</style>
-</head>
-<body>
-<h1>Hello</h1>
-<h3>Request</h3>
-<pre>{$request}</pre>
-<h3>Response</h3>
-<pre>{$response}</pre>
-</body>
-</html>
-EOF;
-        $response->setContent($html);
-        return $response;
+    public function index() {
+        $data = [
+            'msg' => 'これはBladeを利用したサンプルです。',
+        ];
+        return view('hello.index', $data);
     }
 };
