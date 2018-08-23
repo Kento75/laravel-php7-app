b1ee804968a6399715c3c523231ee9c94dbbfe24
Kento75 <kento2github@gmail.com>
2018-08-24 06:25:42 +0900

    Fix from controll logic

--- laravelapp/resources/views/hello/index.blade.php
+++ laravelapp/resources/views/hello/index.blade.php
@@ -8,20 +8,32 @@
 
 @section('content')
   <p>{{ $msg }}</p>
+  @if (count($errors) > 0)
+    <p>入力に問題があります。再入力してください。</p>
+  @endif
   <table>
     <form action="/hello" method="post">
       {{ csrf_field() }}
+      @if ($errors -> has('name'))
+        <tr><th>ERROR</th><td>{{ $errors -> first('name') }}</td><tr>
+      @endif
       <tr>
         <th>name: </th>
-        <td><input type="text" name="name"></td>
+        <td><input type="text" name="name" value="{{ old('name') }}"></td>
       </tr>
+      @if ($errors -> has('mail'))
+        <tr><th>ERROR</th><td>{{ $errors -> first('mail') }}</td><tr>
+      @endif
       <tr>
         <th>mail: </th>
-        <td><input type="text" name="mail"></td>
+        <td><input type="text" name="mail" value="{{ old('mail') }}"></td>
       </tr>
+      @if ($errors -> has('age'))
+        <tr><th>ERROR</th><td>{{ $errors -> first('age') }}</td><tr>
+      @endif
       <tr>
         <th>age: </th>
-        <td><input type="text" name="age"></td>
+        <td><input type="text" name="age" value="{{ old('age') }}"></td>
       </tr>
       <tr>
         <th></th>
