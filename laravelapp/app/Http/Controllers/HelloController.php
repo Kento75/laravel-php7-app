ad1d4387f97f90943851a19f8303b2fb1b874f68
Kento75 <kento2github@gmail.com>
2018-08-25 09:29:50 +0900

    Fix validator

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -3,23 +3,58 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-
+use App\Http\Requests\HelloRequest;
+use Validator;
 
 class HelloController extends Controller
 {
     public function index(Request $request) {
 
-        return view('hello.index', ['msg' => 'フォームを入力：']);
+        $validator = Validator::make($request -> query(), [
+            'id'   => 'required',
+            'pass' => 'required',
+        ]);
+
+        if($validator -> fails()) {
+            $msg = 'クエリーに問題があります。';
+        } else {
+            $msg = 'ID/PASSを受付ました。フォームを入力してください。';
+        }
+
+        return view('hello.index', ['msg' => $msg,]);
     }
 
     public function post(Request $request) {
-        $validate_rule = [
+
+        $rules = [
             'name' => 'required',
             'mail' => 'email',
-            'age'  => 'numeric|between:0, 150',
+            'age' => 'numeric',
         ];
-        $this -> validate($request, $validate_rule);
+
+        $message = [
+            'name.required' => '名前は必ず入力してください。',
+            'mail.email'    => 'メールアドレスが必要です。',
+            'age.numeric'   => '年齢を整数で記入してください。',
+            'age.min'       => '年齢はゼロ歳以上で記入してください。',
+            'age.max'       => '年齢は200歳以下で記入してください。',
+        ];
+
+        $validator = Validator::make($request -> all(), $rules, $message);
+
+        $validator -> sometimes('age', 'min:0', function($input){
+            return !is_int($input -> age);
+        });
+
+        $validator -> sometimes('age', 'max:200', function($input){
+            return !is_int($input -> age);
+        });
+
+        if($validator -> fails()) {
+            return redirect('/hello')
+                -> withErrors($validator)
+                -> withInput();
+        }
         return view('hello.index', ['msg' => '正しく入力されました!']);
     }
 };
--- laravelapp/app/Http/Requests/HelloRequest.php
+++ laravelapp/app/Http/Requests/HelloRequest.php
@@ -13,7 +13,11 @@ class HelloRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        if($this -> path() == 'hello') {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /**
@@ -24,7 +28,20 @@ class HelloRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => 'required',
+            'mail' => 'email',
+            'age' => 'numeric|between :0,150',
         ];
     }
+
+    public function messages()
+    {
+        return [
+            'name.required' => '名前は必ず入力してください。',
+            'mail.email'    => 'メールアドレスが必要です。',
+            'age.numeric'   => '年齢を整数で記入してください。',
+            'age.between'   => '年齢は 0 ~ 150 の間で入力してください。',
+        ];
+    }
+
 }
